050d138644d03be31dc1cae23b1b10d8a9698f7b
Kade Lyons <klyons@seualg.local>
2019-02-22 08:12:40 -0700

    Feb 21 Work

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,4 +155,27 @@
 			delay: 250
 		});
 
+
+
+
+
+
+
+		$(".modal-trigger").click(function(e){
+		  e.preventDefault();
+		  dataModal = $(this).attr("data-modal");
+		  $("#" + dataModal).css({"display":"block"});
+		  // $("body").css({"overflow-y": "hidden"}); //Prevent double scrollbar.
+		});
+
+		$(".close-modal, .modal-sandbox").click(function(){
+		  $(".modal").css({"display":"none"});
+		  // $("body").css({"overflow-y": "auto"}); //Prevent double scrollbar.
+		});
+
+
+
+
+
+
 })(jQuery);
